741b782c10cbc760fe842dc5f952a1d07570f924
E-corp Company Group <e.corpcompanygroup27@gmail.com>
2023-06-06 22:26:40 +0300

    erros are fixed

--- JAVASCRIPT/Main.js
+++ JAVASCRIPT/Main.js
@@ -1,7 +1,10 @@
 let DIV=document.querySelector('div');
 
+//LINKS
+let SPLASHSCREENLINK='https://e-corpcompanygroup.github.io/GiG-WORLD/PAGES/splashScreen.txt'
+
+
+
 //APP LOADING
 SPLASHSCREEN();
 
-//LINKS
-let SPLASHSCREENLINK='https://e-corpcompanygroup.github.io/GiG-WORLD/PAGES/splashScreen.txt'
